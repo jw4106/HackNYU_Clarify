79ceca5c260762a6e4ac14355af8d8c03df68623
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-03-24 12:42:07 -0400

    currently broken

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 //app.js
+const mongoose = require('mongoose');
+const express = require('express');
+const passport = require('passport');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
-//requires
-const path = require("path");
-const express = require("express");
-
-//app
 const app = express();
-//serve static files in public
-app.use(express.static(path.join(__dirname, "public")));
-//set view engine to handlebars
-app.set("views", path.join(__dirname, "views"));
+
+const configDB = require("./config/database.js");
+
+//configuration
+mongoose.connect(configDB.url);
+
+//pass passport for configuration
+require("./config/passport")(passport);
+
+// body parser setup
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "hbs");
 
-//routes
-require("./routes/places.js")(app);
+// required for passport
+app.use(session({ secret: 'hackNYU' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-//set environment variable to the correct port number
+// serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+require("./routes/routes.js")(app, passport);
 app.listen(process.env.PORT || 3000);
+//app.listen(3000);
